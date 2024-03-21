e8250246aca7a82fb7f3d38d336ed9df8fc3ab7c
bensuppanz <benjaminsuppanz@gmail.com>
2024-03-21 18:30:09 +0100

    feat: added `createReport` functionality with relevant functions The method generates the lines which are then written into a markdown file. It uses helper functions to correctly format the page headings and to create the reports for the subpages

--- src/main/java/org/crawler/Printer.java
+++ src/main/java/org/crawler/Printer.java
@@ -1,10 +1,12 @@
 package org.crawler;
 
+import org.crawler.crawl.PageInfo;
 import org.jsoup.nodes.Element;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Printer {
@@ -16,6 +18,45 @@ public class Printer {
         }
 
         writer.close();
+    }
+
+    public static List<String> createReport (PageInfo page) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        lines.add(String.format("input: <%s>", page.getUrl()));
+        lines.add(String.format("language: <%s>", page.getLanguage()));
+        if (page.isBroken()) lines.add("Broken page!");
+
+        lines.addAll(formatPageHeadings(page));
+        lines.addAll(createSubPagesReport(page));
+
+        return lines;
+    }
+
+    private static List<String> formatPageHeadings (PageInfo page) {
+        List<String> headings = new ArrayList<>();
+
+        for (Element heading : page.getHeadings()) {
+            String arrow = ("-").repeat(page.getDepth());
+            if (page.getDepth() > 0) arrow += "> ";
+
+            int level = getHeadingLevel(heading);
+            headings.add(createHeading(level, arrow + heading.text()));
+        }
+
+        return headings;
+    }
+
+    private static List<String> createSubPagesReport (PageInfo page) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        for (PageInfo subPage : page.getSubPagesInfo()) {
+            lines.add("");
+            lines.add("");
+            lines.addAll(createReport(subPage));
+        }
+
+        return lines;
     }
 
     private static int getHeadingLevel (Element heading) {
