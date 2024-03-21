bb4095671f885888a77133bca1de7f78371f3607
bensuppanz <benjaminsuppanz@gmail.com>
2024-03-21 18:41:19 +0100

    feat: implemented main class

---
+++ src/main/java/org/crawler/Main.java
@@ -0,0 +1,23 @@
+package org.crawler;
+
+import org.crawler.config.Configuration;
+import org.crawler.crawl.Crawler;
+import org.crawler.crawl.PageInfo;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        if (!System.getenv().containsKey("API_KEY")) {
+            System.out.println("Translation API-Key not found. Please set the API-Key as an environment variable.");
+            return;
+        }
+
+        Configuration config = Configuration.requestConfiguration();
+//      Configuration config = new Configuration("https://orf.at/", 3, new String[]{}, "german");
+        Crawler crawler = new Crawler(config);
+        PageInfo result = crawler.crawl();
+        Printer.printReport(Printer.createReport(result));
+
+    }
+}
