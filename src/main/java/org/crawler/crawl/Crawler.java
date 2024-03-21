cfe7a776dbe04a0988c5173148767e2ba702fd24
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-21 17:45:45 +0100

    feat: created `Crawler` class
    Created a simple Crawler class that takes in the crawler-config in its constructer.

---
+++ src/main/java/org/crawler/crawl/Crawler.java
@@ -0,0 +1,13 @@
+package org.crawler.crawl;
+
+import org.crawler.config.Configuration;
+
+
+public class Crawler {
+    private Configuration config;
+
+    public Crawler (Configuration config) {
+        this.config = config;
+    }
+
+}
