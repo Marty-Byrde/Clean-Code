28e0909b0bb9c97bde30b7b490cecb3e0dc555e1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-21 17:28:40 +0100

    feat: implemented `Translater` class
    This class comes with a private function that calls the api. It takes in a body argument of type string which is used in the requests-body.
    
    Note: The API-Key must be provided as an enviromental variable.

---
+++ src/main/java/org/crawler/translate/Translater.java
@@ -0,0 +1,27 @@
+package org.crawler.translate;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Translater {
+
+
+    private static String callAPI (String body) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://text-translator2.p.rapidapi.com/translate"))
+                .header("content-type", "application/x-www-form-urlencoded")
+                .header("X-RapidAPI-Key", System.getenv("API_KEY"))
+                .header("X-RapidAPI-Host", "text-translator2.p.rapidapi.com")
+                .method("POST", HttpRequest.BodyPublishers.ofString("source_language=auto" + body))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONObject json = new JSONObject(response.body());
+        return json.getJSONObject("data").getString("translatedText");
+    }
+}
