9835f489403133dde8a8534dbd5700f518f6f6a7
bensuppanz <benjaminsuppanz@gmail.com>
2024-03-21 18:21:59 +0100

    feat: added `getHeadingLevel` functionality The method is used to return the heading level e.g. h3 returns 3

--- src/main/java/org/crawler/Printer.java
+++ src/main/java/org/crawler/Printer.java
@@ -1,7 +1,15 @@
 package org.crawler;
 
+import org.jsoup.nodes.Element;
+
 public class Printer {
 
+
+    private static int getHeadingLevel (Element heading) {
+        if (!heading.tagName().startsWith("h") && heading.tagName().length() != 2) return -1;
+
+        return Integer.parseInt(heading.tagName().split("h")[1]);
+    }
     private static String createHeading(int level, String title) {
         StringBuilder builder = new StringBuilder();
         builder.append("#".repeat(level));
