3a22ec4a1b9361a0400f49eabce25b3268eee6e4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-21 18:23:27 +0100

    test: drafted setup for crawling test-cases
    Drafted the setup mocking the crawling-process. However, to mock the crawling-process, one must pass a mocked version of the Jsoup library to the Crawler, so that the actual page-retrieval can be mocked. This would force us to change the code-structure just test-purposes, which does not seem to be useful.

--- src/test/java/crawl/CrawlerTests.java
+++ src/test/java/crawl/CrawlerTests.java
@@ -4,6 +4,7 @@ import org.crawler.config.Configuration;
 import org.crawler.crawl.Crawler;
 import org.crawler.crawl.PageInfo;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,15 +14,50 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class CrawlerTests {
 
     Crawler crawler;
     Configuration config = new Configuration("https://orf.at/", 3, new String[]{}, "german");
     PageInfo brokenPageInfo = new PageInfo("invalid.url.at", "", new Elements(), new ArrayList<>(), 0);
 
+    PageInfo mockedPageInfo = mock(PageInfo.class);
+
+    Document mockedDocument = mock(Document.class);
+    Element mockedHeading = mock(Element.class);
+    Element mockedAnchor = mock(Element.class);
+
+
     @BeforeEach
     public void setup () {
         crawler = new Crawler(config);
+
+        
+        Element[] mockedPageHeadings = new Element[]{mockedHeading, mockedHeading, mockedHeading};
+        Elements el = new Elements();
+
+        //* Attempt to mock the PageInfo object and the related elements, however this information cannot be used since,
+        //* the basic methods that take in these arguments are private, and the bigger functions fetch the information directly using the Jsoup.connect.get functionality.
+
+        when(mockedPageInfo.getHeadings()).thenReturn(el);
+        when(mockedPageInfo.getUrl()).thenReturn("https://orf.at");
+        when(mockedPageInfo.getLanguage()).thenReturn("de");
+
+        when(mockedPageInfo.getPageLinks()).thenReturn(new ArrayList<>());
+        when(mockedPageInfo.getDepth()).thenReturn(0);
+        when(mockedHeading.tagName()).thenReturn("h2");
+
+        when(mockedHeading.text()).thenReturn("Sample Heading");
+
+
+
+        when(mockedAnchor.attr("href")).thenReturn("http://sample.link.at");
+
+        when(mockedDocument.title()).thenReturn("Sample Title");
+        when(mockedDocument.select("h1, h2, h3, h4, h5, h6")).thenReturn(new Elements());
+        when(mockedDocument.select("a")).thenReturn(new Elements());
     }
 
     @ParameterizedTest
