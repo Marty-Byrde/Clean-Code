8b7489806529f4ad0d1845549f3700521d7071e5
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-21 18:14:47 +0100

    docs: added comment regarding the visibility of a method
    The comment states that the method is public for test-purposes, but would be private otherwise.

--- src/main/java/org/crawler/crawl/Crawler.java
+++ src/main/java/org/crawler/crawl/Crawler.java
@@ -36,6 +36,9 @@ public class Crawler {
         return page;
     }
 
+    /**
+     * @implNote Would be private if it wasn't for testing purposes.
+     */
     public PageInfo retrievePageInfo (String url, String[] domains, int depth) {
         PageInfo result = new PageInfo(url, "", new Elements(), new ArrayList<>(), depth);
         Document document = getDocument(url);
