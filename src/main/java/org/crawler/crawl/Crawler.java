1f858040f0afa33287da3745666bd8b34765cd24
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-05-10 12:21:56 +0200

    refactor: reworked logs inside the Crawler
    Replaced all the System.out logs with the `Console`.`print` method. This way it is clear where the logs come from. Additionally most log-messages were either replaced with more meaningful log-messages or removed.

--- src/main/java/org/crawler/crawl/Crawler.java
+++ src/main/java/org/crawler/crawl/Crawler.java
@@ -9,7 +9,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jsoup.select.NodeFilter;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,16 +23,14 @@ public class Crawler {
     }
 
     public PageInfo crawl (String url) {
+        Console.print("Crawler with id: " + Thread.currentThread().getId(), "is going to crawl", url, "\n");
         return getPage(url, 0, config.getMaxDepth(), config.getDomains());
     }
 
     private PageInfo getPage (String url, int currentDepth, int maxDepth, String[] allowedDomains) {
         if (currentDepth > maxDepth) return null;
-        System.out.println("Crawling-Depth: " + currentDepth);
         PageInfo page = retrievePageInfo(url, allowedDomains, currentDepth);
-
-        List<String> links_to_crawl = page.getPageLinks();
-        System.out.println("-> Page has " + links_to_crawl.size() + " sublinks...");
+        Console.print("[id: " + Thread.currentThread().getId() + "]:", page.getPageLinks().size() + "", "Sublinks found in (depth", (currentDepth) + ")", url);
 
         recursion(page, currentDepth, maxDepth, allowedDomains);
         return page;
@@ -49,9 +46,6 @@ public class Crawler {
         //* In case one cannot connect to the requested URL
         if (document == null) return result;
 
-        System.out.println();
-        System.out.println("Crawling: " + document.title());
-
         result.setLanguage(getSourceLanguage(document));
         result.setHeadings(document.select("h1, h2, h3, h4, h5, h6"));
         result.setPageLinks(removeLinkLoops(url, getFilteredPageLinks(document, domains)));
@@ -61,8 +55,6 @@ public class Crawler {
 
     private void recursion (PageInfo originPage, int currentDepth, int maxDepth, String[] allowedDomains) {
         List<String> links_to_crawl = originPage.getPageLinks();
-        System.out.println("-> Page has " + links_to_crawl.size() + " sublinks...");
-
 
         ArrayList<PageInfo> results = new ArrayList<>();
         for (String link : links_to_crawl) {
@@ -82,10 +74,8 @@ public class Crawler {
         try {
             Connection connection = Jsoup.connect(url);
             return connection.get();
-        } catch (IOException e) {
-            Console.print(Red, "Retrieval was not successful for", url, "due to an IOException.");
-        } catch (Exception ignored) {
-            Console.print(Red, "Retrieving document from", url, "failed.");
+        } catch (Exception e) {
+            Console.print(Red, "Document Retrieval for", url, "failed.");
         }
         return null;
     }
